8c0c3a2f0d70250c080c9aaf6bed8d60438a9534
fluffy-lion <fluffylionprojects@gmail.com>
2023-08-10 12:32:08 +0100

    server and connected to db

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,13 @@ const mongoose = require('mongoose')
 const app = express()
 app.use(express.json())
 // app.use routes
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
 
-app.listen(process.env.PORT, () => {
-    console.log(`listening on port ${process.env.PORT}`)
-})
+        app.listen(process.env.PORT, () => {
+            console.log(`listening on port ${process.env.PORT}, connected to db`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
