b8694216955ab925f83bf87907f4307abcf55d8f
fluffy-lion <fluffylionprojects@gmail.com>
2023-08-18 15:42:15 +0100

    hamburger menu working

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -4,28 +4,35 @@ import './Navbar.css'
 
 const Navbar = () => {
     const [toggle, setToggle] = useState(false)
-   
+
+    const mobileMenu = () => {
+        toggle ? setToggle(false) : setToggle(true)
+    }
+    const itemClicked = () => {
+        toggle ? setToggle(false) : setToggle(true)
+    }
     return (
         <div>
             <header>
-                <nav className={toggle ? "show" : "hide" }>
-                    <button onClick={() => setToggle(!toggle)} className="my-button" id="my-button">
-                        <div className="fas fa-bars"></div>
-                    </button>
-                    <ul id='mynav'>
-                        <li>
-                            <Link className="link" to='/'>
+                <nav className="navbar">
+                    <a href="#" className='nav-logo'>records</a>
+                    <ul className={`nav-menu ${toggle ? "active" : ""}`}>
+                        <li className='nav-item'>
+                            <Link onClick={itemClicked} className="nav-link" to='/'>
                                 home
                             </Link>
                         </li>
-                    </ul>
-                    <ul>
-                        <li>
-                            <Link className="link" to='/saved'>
+                        <li className='nav-item'>
+                            <Link onClick={itemClicked} className="nav-link" to='/saved'>
                                 saved records
                             </Link>
                         </li>
                     </ul>
+                    <div onClick={mobileMenu} className={`hamburger ${toggle ? "active" : ""}`}>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </div>
                 </nav>
             </header>
         </div>
